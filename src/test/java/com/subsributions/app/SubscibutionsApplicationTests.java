2632db50a3301790200ebd22e363e7d09c1a336d
Abriel <abrielz11@gmail.com>
2025-05-07 20:47:20 +0300

    ft added read me, added entity graphs for all 3 entities, added script for flyway, added dtos. added error handler added docker compose, added scratch for docker file and docker-compose

---
+++ src/test/java/com/subsributions/app/SubscibutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subsributions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscibutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
